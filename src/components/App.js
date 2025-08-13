d9e58a12f19ec9cbda821612fae4eb9d8c290574
[Prerit Sahai] <[preritsahai13@gmail.com]>
2025-08-13 23:11:16 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
     return (
         <div id="main">
-            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+            <p>Now I can render any React component on any DOM node I want using ReactDOM.render </p>
         </div>
     );
 };
